fbd1fefdc360a0b5891f5b87ca310e6337d17549
Matysik <boguslaw.matysik@capgemini.com>
2021-02-16 10:24:36 +0100

    Initial commit

---
+++ src/main/java/com/boguslaw/miniproject/MiniProjectApplication.java
@@ -0,0 +1,14 @@
+package com.boguslaw.miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
